b8938d5cdae2be07dd95f509d3ebec7fd70e76e8
Lukasz Piwowarski <lukaszp4@onet.eu>
2024-07-23 11:00:46 +0200

    initial commit of Lotto project

---
+++ src/main/java/pl/lotto/domain/numberreceiver/NumberReceiverFacade.java
@@ -0,0 +1,13 @@
+package pl.lotto.domain.numberreceiver;
+
+/**
+ * Klient podaje 6 liczb
+ * liczby muszą być w zakresie 1-99
+ * liczby nie mogą się powtarzać
+ * klient dostaje informacje o dacie losowania
+ * klient dostaje informacje o swoim unikalnym identyfikatorze losowania
+ **/
+
+public class NumberReceiverFacade {
+    
+}
