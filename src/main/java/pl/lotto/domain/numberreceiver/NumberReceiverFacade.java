8e92af89ffd593a947a2ce97911dc39f2366d44f
Lukasz Piwowarski <lukaszp4@onet.eu>
2024-07-23 15:54:55 +0200

    refactoring of NumberReceiverFacade methods

--- src/main/java/pl/lotto/domain/numberreceiver/NumberReceiverFacade.java
+++ src/main/java/pl/lotto/domain/numberreceiver/NumberReceiverFacade.java
@@ -14,23 +14,17 @@ import java.util.Set;
 public class NumberReceiverFacade {
 
     public String inputNumbers(Set<Integer> numbersFromUser) {
-        List<Integer> filteredNumbers = filterAllNumbersInOfRange(numbersFromUser);
-
-        if (areAllNumbersInRange(filteredNumbers)) {
+        boolean areAllNumbersInRange = areAllNumbersInRange(numbersFromUser);
+        if (areAllNumbersInRange) {
             return "success";
         }
         return "failed";
     }
 
-    private static List<Integer> filterAllNumbersInOfRange(Set<Integer> numbersFromUser) {
+    private boolean areAllNumbersInRange(Set<Integer> numbersFromUser) {
         return numbersFromUser.stream()
                 .filter(number -> number >= 1)
                 .filter(number -> number <= 99)
-                .toList();
+                .count() == 6;
     }
-
-    private boolean areAllNumbersInRange(List<Integer> filteredNumbers) {
-        return filteredNumbers.size() == 6;
-    }
-
 }
